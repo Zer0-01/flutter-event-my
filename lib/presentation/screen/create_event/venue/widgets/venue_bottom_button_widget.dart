144c37ced1123936a325473b2bd4b1d18e2b3b8e
Anas <anas.zulkifli2001@gmail.com>
2025-07-24 23:26:52 +0800

    feat: update

--- lib/presentation/screen/create_event/venue/widgets/venue_bottom_button_widget.dart
+++ lib/presentation/screen/create_event/venue/widgets/venue_bottom_button_widget.dart
@@ -1,55 +1,60 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_my_event/presentation/screen/create_event/venue/bloc/venue_bloc.dart';
 
 class VenueBottomButtonWidget extends StatelessWidget {
   const VenueBottomButtonWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            color: Theme.of(context).colorScheme.outline,
-            width: 1,
+    return BlocBuilder<VenueBloc, VenueState>(
+      builder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                color: Theme.of(context).colorScheme.outline,
+                width: 1,
+              ),
+            ),
           ),
-        ),
-      ),
-      child: BottomAppBar(
-        child: Row(
-          spacing: 8,
-          children: [
-            Expanded(
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                onPressed: () {
-                  context.router.pop();
-                },
-                child: const Text("Previous"),
-              ),
-            ),
-            Expanded(
-              child: FilledButton(
-                style: FilledButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+          child: BottomAppBar(
+            child: Row(
+              spacing: 8,
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      context.router.pop();
+                    },
+                    child: const Text("Previous"),
                   ),
                 ),
-                onPressed: () {},
-                child: const Text("Next"),
-              ),
+                Expanded(
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: state.selectedVenueId.isNegative ? null : () {},
+                    child: const Text("Next"),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }

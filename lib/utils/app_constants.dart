dc1b106ea2991828009b01a903025dac0abf9299
Anas <anas.zulkifli2001@gmail.com>
2025-08-21 23:13:26 +0800

    feat: constant

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,1 +1,21 @@
+class LanguageCodeConstant {
+  static const String en = 'en';
+  static const String ms = 'ms';
+}
+
+class CountryCodeConstant {
+  static const String uk = 'GB';
+  static const String malaysia = 'MY';
+}
+
 enum EventCategoryEnum { wedding, birthday, sport, religious, corporate }
+
+enum CountryEnum {
+  malaysia(LanguageCodeConstant.ms, CountryCodeConstant.malaysia),
+  uk(LanguageCodeConstant.en, CountryCodeConstant.uk);
+
+  final String languageCode;
+  final String countryCode;
+
+  const CountryEnum(this.languageCode, this.countryCode);
+}
